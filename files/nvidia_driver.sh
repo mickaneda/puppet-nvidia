040ece54801a2685ae8c7e1c0955e2dccf41aa4b
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-03-21 02:51:10 +0900

    nvidia driver installation twice

--- files/nvidia_driver.sh
+++ files/nvidia_driver.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+if [ -f /proc/driver/nvidia/version ];then
+  rm -f $0
+  exit
+fi
+
 url=$1
 script=$(basename $url)
 curl -LO "$url"
 chmod 755 $script
+
+# Need twice to load nvidia-uvm
+./$script --accept-license --silent --disable-nouveau  --no-opengl-files --no-libglx-indirect --dkms
 ./$script --accept-license --silent --disable-nouveau  --no-opengl-files --no-libglx-indirect --dkms
 
 ret=$?
