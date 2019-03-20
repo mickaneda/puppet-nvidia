756fcb27fd864a6616ece68d802a836cfb5a111c
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2019-03-21 02:33:34 +0900

    added --no-opengl-files --no-libglx-indirect

--- files/nvidia_driver.sh
+++ files/nvidia_driver.sh
@@ -3,7 +3,8 @@ url=$1
 script=$(basename $url)
 curl -LO "$url"
 chmod 755 $script
-./$script --accept-license --silent --disable-nouveau --dkms
+./$script --accept-license --silent --disable-nouveau  --no-opengl-files --no-libglx-indirect --dkms
+
 ret=$?
 rm -f $script
 rm -f $0
